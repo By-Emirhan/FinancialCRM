2f8a37b9aa32d65085c8dc3d6f7db6e5f61893a6
By-Emirhan <doganlardan190@gmail.com>
2025-04-05 12:17:14 +0300

    Fİnal Case

---
+++ FinancialCRM/databases.sql
@@ -0,0 +1,108 @@
+﻿USE [FinancialCRMDb]
+GO
+/****** Object:  Table [dbo].[BankProcesses]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[BankProcesses](
+	[BankProcessID] [int] IDENTITY(1,1) NOT NULL,
+	[Description] [nvarchar](250) NULL,
+	[ProcessDate] [date] NULL,
+	[ProcessType] [nvarchar](50) NULL,
+	[Amount] [decimal](18, 2) NULL,
+	[BankID] [int] NULL,
+ CONSTRAINT [PK_BankProcesses] PRIMARY KEY CLUSTERED 
+(
+	[BankProcessID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Banks]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Banks](
+	[BankID] [int] IDENTITY(1,1) NOT NULL,
+	[BankAccountNumber] [nvarchar](50) NULL,
+	[BankTitle] [nvarchar](50) NULL,
+	[BankBalance] [decimal](18, 2) NULL,
+ CONSTRAINT [PK_Banks] PRIMARY KEY CLUSTERED 
+(
+	[BankID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Bills]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Bills](
+	[BiLLID] [int] IDENTITY(1,1) NOT NULL,
+	[BillTitle] [nvarchar](50) NULL,
+	[BillAmount] [decimal](18, 2) NULL,
+	[BillPeriod] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Bills] PRIMARY KEY CLUSTERED 
+(
+	[BiLLID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Categories]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Categories](
+	[CategoryID] [int] IDENTITY(1,1) NOT NULL,
+	[CategoryName] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Categories] PRIMARY KEY CLUSTERED 
+(
+	[CategoryID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Spendings]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Spendings](
+	[SpendingID] [int] IDENTITY(1,1) NOT NULL,
+	[SpendingTitle] [nvarchar](250) NULL,
+	[SpendingAmount] [decimal](18, 2) NULL,
+	[SpendingDate] [date] NULL,
+	[CategoryID] [int] NULL,
+ CONSTRAINT [PK_Spendings] PRIMARY KEY CLUSTERED 
+(
+	[SpendingID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 5.04.2025 12:15:39 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[UserID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [nvarchar](50) NULL,
+	[Password] [nvarchar](50) NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[UserID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[BankProcesses]  WITH CHECK ADD  CONSTRAINT [FK_BankProcesses_Banks] FOREIGN KEY([BankID])
+REFERENCES [dbo].[Banks] ([BankID])
+GO
+ALTER TABLE [dbo].[BankProcesses] CHECK CONSTRAINT [FK_BankProcesses_Banks]
+GO
+ALTER TABLE [dbo].[Spendings]  WITH CHECK ADD  CONSTRAINT [FK_Spendings_Categories] FOREIGN KEY([CategoryID])
+REFERENCES [dbo].[Categories] ([CategoryID])
+GO
+ALTER TABLE [dbo].[Spendings] CHECK CONSTRAINT [FK_Spendings_Categories]
+GO
